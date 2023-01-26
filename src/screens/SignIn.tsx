d90ab1debae5f7aff584cb06274dabfad2b277f3
Mateus Holanda <mateus23ita@gmail.com>
2023-01-26 00:19:40 -0300

    Create Input element

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,31 @@
-import { VStack, Heading } from 'native-base';
+import { VStack, Heading, Icon, useTheme } from 'native-base';
+import { Envelope, Key } from 'phosphor-react-native';
+
+import Logo from '../assets/logo_primary.svg';
+
+import { Input } from '../components/Input';
 
 export function SignIn() {
+  const { colors } = useTheme();
+
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
         SignIn
       </Heading>
+
+      <Input
+        placeholder="E-mail"
+        InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+        mb={4}
+      />
+      <Input
+        placeholder="Password"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
+      />
     </VStack>
   )
 }
