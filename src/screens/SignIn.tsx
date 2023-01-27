bcf2a2b03360ff2f21abee30157a6c521a92db8f
Mateus Holanda <mateus23ita@gmail.com>
2023-01-26 22:46:17 -0300

    Create Home screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
 
 import Logo from '../assets/logo_primary.svg';
 
 import { Input } from '../components/Input';
+import { Button } from '../components/Button';
 
 export function SignIn() {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  
   const { colors } = useTheme();
+
+  function handleSignIn() {
+
+  }
 
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -17,15 +26,20 @@ export function SignIn() {
       </Heading>
 
       <Input
+        mb={4}
         placeholder="E-mail"
         InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
-        mb={4}
+        onChangeText={setName}
       />
       <Input
+        mb={8}
         placeholder="Password"
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         secureTextEntry
+        onChangeText={setPassword}
       />
+
+      <Button title="Enter" w="full" onPress={handleSignIn} />
     </VStack>
   )
 }
