c9369cafe010941fa54ae526479194a339c3ff69
Mateus Holanda <mateus23ita@gmail.com>
2023-02-02 23:00:31 -0300

    Integration with Firestore collection and implementation of Order Details page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,27 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Alert } from 'react-native';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 import { useNavigation } from '@react-navigation/native';
 import { VStack, HStack, Heading, IconButton, Text, Center, FlatList, useTheme } from 'native-base';
 import { SignOut, ChatTeardropText } from 'phosphor-react-native';
 
+import { dateFormat } from '../utils/firestoreDateFormat';
+
 import Logo from '../assets/logo_secondary.svg';
 
+import { Loading } from '../components/Loading';
 import { Filter } from '../components/Filter';
 import { Button } from '../components/Button';
 import { Order, OrderProps } from '../components/Order';
 
 
 export function Home() {
+  const [isLoading, setIsLoading] = useState(true);
   const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
-  const [orders, setOrders] = useState<OrderProps[]>([
-    {
-      id: '123',
-      property: '4235435',
-      when: '01/26/2023 at 12:44:33',
-      status: 'open'
-    }
-  ]);
+  const [orders, setOrders] = useState<OrderProps[]>([]);
 
   const navigation = useNavigation();
   const { colors } = useTheme();
@@ -40,6 +38,32 @@ export function Home() {
       return Alert.alert('Signout', 'Error while signing out.')
     });
   }
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const subscriber = firestore()
+      .collection('orders')
+      .where('status', '==', statusSelected)
+      .onSnapshot(snapshot => {
+        const data = snapshot.docs.map(doc => {
+          const { property, description, status, created_at } = doc.data();
+
+          return {
+            id: doc.id,
+            property,
+            description,
+            status,
+            when: dateFormat(created_at)
+          }
+        });
+
+        setOrders(data);
+        setIsLoading(false);
+      });
+    
+    return subscriber;
+  }, [statusSelected]);
 
   return (
     <VStack flex={1} pb={6} bg="gray.700">
@@ -92,22 +116,25 @@ export function Home() {
           />
         </HStack>
 
-        <FlatList
-          data={orders}
-          keyExtractor={item => item.id}
-          renderItem={({ item }) => <Order data={item} onPress={() => handleOpenDetails(item.id)} />}
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{ paddingBottom: 100 }}
-          ListEmptyComponent={() => (
-            <Center>
-              <ChatTeardropText color={colors.gray[300]} size={40} />
-              <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
-                You don't have any {'\n'}
-                { statusSelected === 'open' ? 'open' : 'closed' } requests for now
-              </Text>
-            </Center>
-          )}
-        />
+        {
+          isLoading ? <Loading /> :
+          <FlatList
+            data={orders}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => <Order data={item} onPress={() => handleOpenDetails(item.id)} />}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: 100 }}
+            ListEmptyComponent={() => (
+              <Center>
+                <ChatTeardropText color={colors.gray[300]} size={40} />
+                <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                  You don't have any {'\n'}
+                  { statusSelected === 'open' ? 'open' : 'closed' } requests for now
+                </Text>
+              </Center>
+            )}
+          />
+        }
 
         <Button title="New Request" onPress={handleNewRequest} />
       </VStack>
--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,10 +1,42 @@
+import { useState } from 'react';
+import { Alert } from 'react-native';
 import { VStack } from 'native-base';
+import { useNavigation } from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
 
 import { Header } from '../components/Header';
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 
 export function Register() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [property, setProperty] = useState('');
+  const [description, setDescription] = useState('');
+
+  const navigation = useNavigation();
+
+  function handleNewOrderRegister() {
+    if (!property || !description) {
+      return Alert.alert('Register', 'Please, fill in all fields.');
+    }
+
+    setIsLoading(true);
+
+    firestore().collection('orders').add({
+      property,
+      description,
+      status: 'open',
+      created_at: firestore.FieldValue.serverTimestamp()
+    }).then(() => {
+      Alert.alert('Request', 'Request registered successfully!');
+      navigation.goBack();
+    }).catch((error) => {
+      console.log(error);
+      setIsLoading(false);
+      return Alert.alert('Request', 'Something went wrong while creating your request.');
+    })
+  }
+
   return (
     <VStack flex={1} p={6} bg="gray.600">
       <Header title="New request" />
@@ -12,6 +44,7 @@ export function Register() {
       <Input
         placeholder="Property Number"
         mt={4}
+        onChangeText={setProperty}
       />
 
       <Input
@@ -20,9 +53,15 @@ export function Register() {
         mt={5}
         multiline
         textAlignVertical="top"
+        onChangeText={setDescription}
       />
 
-      <Button title="Register" mt={5} />
+      <Button
+        title="Register"
+        mt={5}
+        isLoading={isLoading}
+        onPress={handleNewOrderRegister}
+      />
     </VStack>
   );
 }
